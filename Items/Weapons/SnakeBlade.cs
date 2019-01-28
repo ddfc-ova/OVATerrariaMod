f3d5c80018a583485c4344f0d55c723315383205
Supared <zlb7573@gmail.com>
2019-01-28 03:09:19 -0500

    yeet

--- Items/Weapons/SnakeBlade.cs
+++ Items/Weapons/SnakeBlade.cs
@@ -36,13 +36,6 @@ namespace OVATerrariamod.Items.Weapons
 			recipe.SetResult(this);
 			recipe.AddRecipe();
 		}
-		
-		public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit)
-		{
-			// Add Onfire buff to the NPC for 1 second
-			// 60 frames = 1 second
-			target.AddBuff(BuffID.OnFire, 60);
-		}
 
 	}
 }
