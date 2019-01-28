d2945239c81eb8fda71c6bd83bf3f217ac48fe2c
Supared <zlb7573@gmail.com>
2019-01-28 02:51:15 -0500

    ok

---
+++ Items/Weapons/SnakeBlade.cs
@@ -0,0 +1,48 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OVATerrariamod.Items.Weapons
+{
+	public class SnakeBlade : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Snakeblade");
+			Tooltip.SetDefault("It sticks it's tounge out at you.");
+		}
+		public override void SetDefaults()
+		{
+			item.damage = 24;
+			item.melee = true;
+			item.width = 64;
+			item.height = 64;
+			item.useTime = 21;
+			item.useAnimation = 21;
+			item.useStyle = 1;
+			item.knockBack = 6;
+			item.value = 1000;
+			item.rare = 2;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = false;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Emerald, 8);
+			recipe.AddIngredient(ItemID.JungleSpores, 8);
+			recipe.AddIngredient(ItemID.Stinger, 15);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+		
+		public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit)
+		{
+			// Add Onfire buff to the NPC for 1 second
+			// 60 frames = 1 second
+			target.AddBuff(BuffID.OnFire, 60);
+		}
+
+	}
+}
