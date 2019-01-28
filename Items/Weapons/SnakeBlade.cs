2cf2743b54a23dfaf727aad51bf53b250504518f
Calmaxys2 <calmaxys@gmail.com>
2019-01-28 16:23:23 +0000

    Updated SnakeBlade to include Supa's desired poison effect - fixed start of code to allow for this.

--- Items/Weapons/SnakeBlade.cs
+++ Items/Weapons/SnakeBlade.cs
@@ -1,5 +1,7 @@
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
 
 namespace OVATerrariamod.Items.Weapons
 {
@@ -36,6 +38,10 @@ namespace OVATerrariamod.Items.Weapons
 			recipe.SetResult(this);
 			recipe.AddRecipe();
 		}
-
+		public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit) {
+			// Add Onfire buff to the NPC for 1 second
+			// 60 frames = 1 second
+			target.AddBuff(BuffID.Poisoned, 60);
+		}
 	}
 }
