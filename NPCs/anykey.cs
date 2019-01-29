14aa1bdb4b93fc55b551579570fa94f890cfb623
Supared <zlb7573@gmail.com>
2019-01-29 00:24:41 -0500

    Typo fixed on the Pierogies item, Any npc now has a working shop and sells Pierogies

--- NPCs/anykey.cs
+++ NPCs/anykey.cs
@@ -90,6 +90,15 @@ namespace OVATerrariaMod.NPCs
             }*/
         }
 
+                public override void OnChatButtonClicked(bool firstButton, ref bool openShop) //Allows you to make something happen whenever a button is clicked on this town NPC's chat window. The firstButton parameter tells whether the first button or second button (button and button2 from SetChatButtons) was clicked. Set the shop parameter to true to open this NPC's shop.
+        {
+ 
+            if (firstButton)
+            {
+                openShop = true;   //so when you click on buy button opens the shop
+            }
+        }
+
         public override string GetChat() {
             int partyGirl = NPC.FindFirstNPC(NPCID.PartyGirl);
             if (partyGirl >= 0 && Main.rand.NextBool(4)) {
@@ -130,10 +139,11 @@ namespace OVATerrariaMod.NPCs
             button = Language.GetTextValue("LegacyInterface.28");
         }
 
-        /* public override void SetupShop(Chest shop, ref int nextSlot) {
-            shop.item[nextSlot].SetDefaults(mod.ItemType("ExampleItem"));
-            nextSlot++;
-            shop.item[nextSlot].SetDefaults(mod.ItemType("EquipMaterial"));
+         public override void SetupShop(Chest shop, ref int nextSlot) 
+         {
+            shop.item[nextSlot].SetDefaults(mod.ItemType("Pierogies"));
+            nextSlot++;}
+           /*  shop.item[nextSlot].SetDefaults(mod.ItemType("EquipMaterial"));
             nextSlot++;
             shop.item[nextSlot].SetDefaults(mod.ItemType("BossItem"));
             nextSlot++;
