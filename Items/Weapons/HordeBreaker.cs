6c77373a25a4511a30dde254de3791425bf16698
Supared <zlb7573@gmail.com>
2019-02-20 03:24:21 -0500

    Horde Breaker works. Thank god. And deelon

---
+++ Items/Weapons/HordeBreaker.cs
@@ -0,0 +1,78 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OVATerrariaMod.Items.Weapons
+{
+    public class HordeBreaker : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Horde Breaker");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 10;
+			item.ranged = true; // For Ranged Weapon
+			item.width = 16;
+            item.height = 24;
+            item.useTime = 4;
+			item.reuseDelay = 18;
+            item.useAnimation = 12;
+            item.useStyle = 5; // Bow Use Style
+            item.noMelee = true; // Doesn't deal damage if an enemy touches at melee range.
+            item.value = 10000; // Another way to handle value of item.
+            item.rare = 2;
+            item.UseSound = SoundID.Item5; // Sound for Bows
+            item.useAmmo = AmmoID.Arrow; // The ammo used with this weapon
+            item.shoot = 1;
+            item.shootSpeed = 60f;
+            item.autoReuse = true;
+        }
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            Vector2 muzzleOffset = Vector2.Normalize(new Vector2(speedX, speedY)) * 40f;
+            if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+            {
+                position += muzzleOffset;
+            }
+            
+            float numberProjectiles = 3 + Main.rand.Next(1); // 3, 4, or 5 shots
+            float rotation = MathHelper.ToRadians(20);
+            position += Vector2.Normalize(new Vector2(speedX, speedY)) * 0f;
+            for (int i = 0; i < numberProjectiles; i++)
+            {
+                Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numberProjectiles - 1))) * .2f; // Watch out for dividing by 0 if there is only 1 projectile.
+                Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+            }            
+
+            return false;
+        }
+
+		
+		
+    		public override bool ConsumeAmmo(Player player)
+		{
+			// Because of how the game works, player.itemAnimation will be 11, 7, and finally 3. (UseAmination - 1, then - useTime until less than 0.) 
+			// We can get the Clockwork Assault Riffle Effect by not consuming ammo when itemAnimation is lower than the first shot.
+			return !(player.itemAnimation < item.useAnimation - 2);
+		}
+        		// Help, my gun isn't being held at the handle! Adjust these 2 numbers until it looks right.
+		public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(9, 0);
+		}
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Emerald, 6);
+			recipe.AddIngredient(ItemID.SilverBar, 14);
+			recipe.AddIngredient(ItemID.Bone, 6);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
