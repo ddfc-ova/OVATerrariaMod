627438843501ab0086a07fc00603e97e659ac050
Supared <zlb7573@gmail.com>
2019-02-21 08:28:56 -0500

    added the spas 12, fixed Dredgens Longbow sprite

---
+++ Items/Weapons/spas12.cs
@@ -0,0 +1,59 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OVATerrariaMod.Items.Weapons
+{
+    public class spas12 : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("It's a Spas-12, it needs no further explaination.");
+            DisplayName.SetDefault("Spas 12");
+        }
+
+ 		public override void SetDefaults()
+		{
+			item.damage = 32;
+			item.ranged = true;
+			item.width = 40;
+			item.height = 20;
+			item.useTime = 50;
+			item.useAnimation = 50;
+			item.useStyle = 5;
+			item.noMelee = true; //so the item's animation doesn't do damage
+			item.knockBack = 4;
+			item.value = 10000;
+			item.rare = 2;
+			item.UseSound = SoundID.Item36;
+			item.autoReuse = false;
+			item.shoot = 10; //idk why but all the guns in the vanilla source have this
+			item.shootSpeed = 16f;
+			item.useAmmo = AmmoID.Bullet;
+		}
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			int numberProjectiles = 7 + Main.rand.Next(2); // 4 or 5 shots
+			for (int i = 0; i < numberProjectiles; i++)
+			{
+				Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(15)); // 30 degree spread.
+				// If you want to randomize the speed to stagger the projectiles
+				// float scale = 1f - (Main.rand.NextFloat() * .3f);
+				// perturbedSpeed = perturbedSpeed * scale; 
+				Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+			}
+			return false; // return false because we don't want tmodloader to shoot projectile
+		}
+        		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.TitaniumBar, 12);
+			recipe.AddIngredient(ItemID.Shotgun, 1);
+			recipe.AddIngredient(ItemID.SoulofFright, 15);
+			recipe.AddTile(TileID.MythrilAnvil);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
